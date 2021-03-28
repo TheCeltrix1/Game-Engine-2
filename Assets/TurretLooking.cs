9c2f28c602a941caac0d49b70f43ede984880bd9
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-03-28 16:01:28 +0100

    Turret Follow

--- Assets/TurretLooking.cs
+++ Assets/TurretLooking.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class TurretLooking : MonoBehaviour
 {
     public Transform target;
+    public float clampDegrees;
 
     void FixedUpdate()
     {
-        transform.LookAt(target);
+        Quaternion face = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(target.position, Vector3.up), 10);
+        //face.eulerAngles = new Vector3(Mathf.Clamp(face.eulerAngles.x, -clampDegrees, clampDegrees), face.eulerAngles.y, face.eulerAngles.z);
+        Debug.Log(face.eulerAngles.x);
+        transform.rotation = face;
     }
 }
