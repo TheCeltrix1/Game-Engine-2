5586d12d3dd98035b0450a52bdb73478df496d3e
TheCeltrix1 <60694151+TheCeltrix1@users.noreply.github.com>
2021-04-02 17:31:52 +0100

    Scene 3

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public float rotationPerSecond;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z + (rotationPerSecond * Time.deltaTime));
+    }
+}
